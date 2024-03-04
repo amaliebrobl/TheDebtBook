3089a385a060e39f14c5e66cce9c4dd0254286f4
Amalie Bjørndal Robl <202108833@post.au.dk>
2024-03-04 13:44:18 +0100

    Database samt Debtor klasse med properties er tilføjet

---
+++ TheDebtBook/Database/Database.cs
@@ -0,0 +1,57 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheDebtBook.Models;
+using TheDebtBook.Pages;
+
+namespace TheDebtBook.Database
+{
+    internal class Database
+    {
+        private readonly SQLiteAsyncConnection _connection;
+
+        public Database()
+        {
+            var dataDir = FileSystem.AppDataDirectory;
+            var databasePath = Path.Combine(dataDir, "TheDebtBook.db");
+            var dbOptions = new SQLiteConnectionString(databasePath, true);
+
+            _connection = new SQLiteAsyncConnection(dbOptions);
+
+            _ = Initialise();
+        }
+
+
+        // Laver en ny "Debtor" i databasen
+        private async Task Initialise()
+        {
+            await _connection.CreateTableAsync<Debtor>();
+        }
+
+        // Adder 
+        public async Task<List<Debtor>> GetDebtors()
+        {
+            return await _connection.Table<Debtor>().ToListAsync();
+        }
+
+        public async Task<Debtor> GetName(string name)
+        {
+            var query = _connection.Table<Debtor>().Where(t => t.Name == name);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<double> AddValue(Debtor value)
+        {
+            return await _connection.InsertAsync(value);
+        }
+
+        public async Task<int> UpdateDebtorList(Debtor debtor)
+        {
+            return await _connection.UpdateAsync(debtor);
+        }
+    }
+}
---
+++ TheDebtBook/ViewModels/MainViewModels.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TheDebtBook.ViewModels
+{
+    class MainViewModels : INotifyPropertyChanged
+    {
+        public MainViewModels()
+        {
+            _database = new Database();
+
+            AddTodoCommand = new Command(async () => await AddNewTodo());
+            CompleteTodoCommand = new Command<TodoItem>(async (item) => await CompleteTodo(item));
+            SwipeCompleteTodoCommand = new Command<TodoItem>(async (item) => await SwipeCompleteTodoExecute(item));
+            DeleteTodoCommand = new Command<TodoItem>(async (item) => await DeleteTodo(item));
+            _ = Initialize();
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void RaisePropertyChanged(params string[] properties)
+        {
+            foreach (var propertyName in properties)
+            {
+                PropertyChanged?.Invoke(this, new
+                PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+        public ObservableCollection<TodoItem> Todos { get; set; } = new();
+        public string NewTodoTitle { get; set; } = string.Empty;
+        public DateTime NewTodoDue { get; set; } = DateTime.Now;
+        public ICommand AddTodoCommand { get; set; }
+        public ICommand CompleteTodoCommand { get; set; }
+        public ICommand SwipeCompleteTodoCommand { get; set; }
+        public ICommand DeleteTodoCommand { get; set; }
+        private readonly Database _database;
+
+        #region Methods
+        private async Task Initialize()
+        {
+            var todos = await _database.GetTodos();
+            foreach (var todo in todos)
+            {
+                Todos.Add(todo);
+            }
+        }
+
+        public async Task AddNewTodo()
+        {
+            var todo = new TodoItem
+            {
+                Due = NewTodoDue,
+                Title = NewTodoTitle
+            };
+            var inserted = await _database.AddTodo(todo);
+            if (inserted != 0)
+            {
+                Todos.Add(todo);
+                NewTodoTitle = String.Empty;
+                NewTodoDue = DateTime.Now;
+                RaisePropertyChanged(nameof(NewTodoDue), nameof(NewTodoTitle));
+            }
+        }
+
+        public async Task CompleteTodo(TodoItem todoitem)
+        {
+            var completed = await _database.UpdateTodo(todoitem);
+            OnPropertyChanged(nameof(Todos));
+        }
+
+        public async Task SwipeCompleteTodoExecute(TodoItem todoitem)
+        {
+            todoitem.Done = true;
+            var completed = await _database.UpdateTodo(todoitem);
+        }
+
+        public async Task DeleteTodo(TodoItem todoitem)
+        {
+            var deleted = await _database.DeleteTodo(todoitem);
+            if (deleted != 0)
+            {
+                Todos?.Remove(todoitem);
+            }
+        }
+
+        #endregion Methods
+    }
+}
