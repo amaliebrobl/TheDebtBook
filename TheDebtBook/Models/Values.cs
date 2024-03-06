877b3dd2592a2b14e98cf549aaf7173beccc860d
Severgjh <202001079@post.au.dk>
2024-03-06 13:46:45 +0100

    Values model lavet

--- TheDebtBook/Models/Values.cs
+++ TheDebtBook/Models/Values.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace TheDebtBook.Models
 {
     public class Values
     {
+        [PrimaryKey, AutoIncrement]
+        public int ValueId { get; set; }
+        public int TotalValue { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+
     }
 }
