ea3df5c45960cb29aa15b207239b838d4446ac0b
Severgjh <202001079@post.au.dk>
2024-03-06 15:26:43 +0100

    ValuesListViewModel

--- TheDebtBook/Pages/RegisteredList.xaml.cs
+++ TheDebtBook/Pages/RegisteredList.xaml.cs
@@ -2,7 +2,7 @@ namespace TheDebtBook.Pages;
 
 public partial class RegisteredList : ContentPage
 {
-	public RegisteredList()
+	public RegisteredList(int Id)
 	{
 		InitializeComponent();
 	}
---
+++ TheDebtBook/ViewModels/ValuesListModelView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheDebtBook.Data;
+using TheDebtBook.Models;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using TheDebtBook.Pages;
+
+namespace TheDebtBook.ViewModels
+{
+    public partial class ValuesListModelView : ObservableObject
+    {
+        private readonly Database _database = new Database();
+        private INavigation _navigation;
+
+        [ObservableProperty] public List<Debtor> _debtors;
+
+        [RelayCommand]
+        public void LoadData()
+        {
+            LoadDataAsync();
+        }
+
+        [RelayCommand]
+        public void ViewDebtor(int debtorId)
+        {
+            if (debtorId == 0)
+            {
+                return;
+            }
+
+            _navigation.PushAsync(new RegisteredList(debtorId));
+        }
+
+        public ValuesListModelView(INavigation navigation)
+        {
+            _navigation = navigation;
+            LoadDataAsync();
+        }
+
+        private async void LoadDataAsync()
+        {
+            var debtorDatabase = await _database.GetDebtors();
+            Debtors = new List<Debtor>(debtorDatabase);
+        }
+
+
+
+
+
+
+    }
+}
